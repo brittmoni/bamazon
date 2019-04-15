9040c479f232151f4cf962b2aeca2869573b1cd1
Brittany F <brittfranklin92@gmail.com>
2019-04-14 23:36:41 -0400

    Continued updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,23 +11,50 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  // items();
+  items();
 });
 
-// function items() {
-//   inquirer
-//     .prompt({
+var itemSelected;
 
-//     })
-// }
+function items() {
+  connection.query('SELECT * FROM products', function(err, res){
+    if (err) throw err;
 
-connection.query('SELECT * FROM products', function(err, res){
-  if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + ' | ' + res[i].stock_quantity);
+    }
+    console.log("-----------------------------------");
 
-  for (var i = 0; i < res.length; i++) {
-    console.log(res[i].item_id + " " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + ' | ' + res[i].stock_quantity);
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'product-ID',
+          message: 'What is the ID of the product that you would like to purchase?'
+        }
+      ])
+      .then(function(answer) {
+        itemSelected = answer.product-ID;
+        purchaseAmount();
+      });
+    });
   }
-  console.log("-----------------------------------");
-});
 
-connection.end();
+  function purchaseAmount() {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'items',
+          message: 'How many of this item would you like to purchase?'
+        }
+      ])
+      .then(function(answer) {
+        connection.query('UPDATE products SET stock_quantity=stock_quantity -', answer.items, 'WHERE item_id = ', itemSelected) {
+          if 
+        }
+
+      })
+  }
+
+// connection.end();
