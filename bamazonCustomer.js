ab6bb9a8222528fce641aee1f258ee06586c1ff5
Brittany F <brittfranklin92@gmail.com>
2019-04-14 20:38:48 -0400

    Changes to JavaScript

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,22 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  items();
+});
+
+function items() {
+  inquirer
+    .prompt({
+      
+    })
+}
